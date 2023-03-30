5ab6b31a6b88644b1930bfb871b6d99383cc064f
Kethiri <mohamed.kethiri@epita.com>
2023-03-31 01:02:29 +0200

    psh

--- src/Command.java
+++ src/Command.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public interface Command {
-	public String name();
-	
-	public boolean run(Scanner sc);
+    public String name();
+    public boolean run(Scanner sc);
 }
 
--- src/Quit.java
+++ src/Quit.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class Quit implements Command {
-	@Override
-	public String name() {
-		return "quit";
-	}
+    @Override
+    public String name() {
+        return "quit";
+    }
 
-	@Override
-	public boolean run(Scanner sc) {
-		return true;
-	}
+    @Override
+    public boolean run(Scanner sc) {
+        return false;
+    }
 }
+
