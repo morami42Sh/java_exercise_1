f797c97476c832d6ae3099e067d451f3bff44a01
Kethiri <mohamed.kethiri@epita.com>
2023-03-31 01:10:31 +0200

    psh

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
 
 public class Launcher {
-	public static final String WELCOME = "Bienvenue !";
-	public static final Command[] COMMANDS = {new Fibo(), new Freq(), new Predict(), new Quit()};
+    public static final String WELCOME = "Bienvenue !";
+    public static final Command[] COMMANDS = {new Fibo(), new Freq(), new Predict(), new Quit()};
 
-	public static void main(String[] args) {
-		System.out.println(WELCOME);
-		Scanner sc = new Scanner(System.in);
-		String var = "";
-		Command cmd = null;
-		do {
-			var = sc.nextLine(); // Lire la commande de l'utilisateur
-			cmd = null;
-			for(Command c : COMMANDS) if(c.name().contentEquals(var)) { // Vérifier si la commande est valide
-				cmd = c;
-				break;
-			}
-			if(cmd == null) {
-				System.out.println("Unknown command"); // Si la commande est invalide, afficher un message d'erreur
-			}
-		} while(cmd == null || !cmd.run(sc)); // Tant que la commande est invalide ou n'est pas terminée, continuer la boucle
-		sc.close(); // Fermer le scanner
-	}
+    public static void main(String[] args) {
+        System.out.println(WELCOME);
+        Scanner scanner = new Scanner(System.in);
+        Command command = null;
+        do {
+            String input = scanner.nextLine(); // Lire la commande de l'utilisateur
+            command = null;
+            for (Command c : COMMANDS) {
+                if (c.name().equalsIgnoreCase(input)) { // Vérifier si la commande est valide (ignorer la casse)
+                    command = c;
+                    break;
+                }
+            }
+            if (command == null) {
+                System.out.println("Unknown command"); // Si la commande est invalide, afficher un message d'erreur
+            } else {
+                boolean shouldQuit = command.run(scanner); // Exécuter la commande
+                if (shouldQuit) {
+                    System.out.println("Exiting program...");
+                    System.exit(0); // Quitter le programme si la commande le demande
+                }
+            }
+        } while (command == null || !command.name().equalsIgnoreCase("quit")); // Continuer tant que la commande n'est pas "quit"
+        scanner.close(); // Fermer le scanner
+    }
 }
 
