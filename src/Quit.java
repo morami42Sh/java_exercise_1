effc651b6a51438ba544f7a4368bf6c287079ed5
Kethiri <mohamed.kethiri@epita.com>
2023-03-31 01:29:41 +0200

    psh

--- src/Quit.java
+++ src/Quit.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
 public class Quit implements Command {
-@Override
-public String name() {
-return "quit";
+    @Override
+    public String name() {
+        return "quit";
+    }
+    
+    @Override
+    public boolean run(Scanner scanner) {
+        System.out.println("Sortie du programme...");
+        return true;
+    }
 }
-@Override
-public boolean run(Scanner scanner) {
-    System.out.println("Exiting program...");
-    return true;
-}
-}
+
